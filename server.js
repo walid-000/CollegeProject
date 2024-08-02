e8493fd72e5c536cb0d9a89fe11fc8a1369f7d76
walid <walidshaikh01@gmail.com>
2024-08-02 08:19:29 +0530

    server working properly

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
+const { log } = require("console");
 const express = require("express");
+require('dotenv').config();
 const path = require("path");
-
-
+const port = process.env.PORT || 3000;
 const app = express();
+app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use()
+
 
 app.get("/" , (req , res)=>{
-    res.sendFile()
+    res.sendFile(path.join(__dirname, 'public', 'home.html'));
 });
+
+app.listen(port , ()=>{
+    console.log(`listening at port ${port} `);
+    console.log(`localhost:${port}`);
+})
+
